21af4daec4d587593167edb387da836c76c503c8
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-04-11 16:21:37 -0300

    hero add .

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const typedElements = document.querySelectorAll('.typed');
+  typedElements.forEach(typed => {
+    let typed_strings = typed.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+
+    new Typed(typed, {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      onComplete: (self) => {
+        const typedElement = self.el;
+        typedElement.classList.add('animate__animated', 'animate__fadeOut');
+        setTimeout(() => {
+          self.reset();
+          typedElement.classList.remove('animate__animated', 'animate__fadeOut');
+        }, 1000); // Tempo de espera após a animação de desaparecimento
+      }
+    });
+  });
+});
