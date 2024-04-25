e5d8499fb95087bed5e88eac881d70b80ce58c1b
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-04-25 13:32:20 -0300

    adicionado nova função, envio de email com API

--- js/main.js
+++ js/main.js
@@ -49,3 +49,85 @@ function scrollSuave() {
 }
 
 scrollSuave();
+
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
+function ajustarCols() {
+  const larguraTela = window.innerWidth;
+  let cols = 0;
+
+  if (larguraTela >= 1000) {
+    cols = 85; // Ajuste conforme necessário
+    
+  } else if(larguraTela <=1000 && larguraTela >= 700){
+        cols = 50
+    } 
+    else if (larguraTela <= 700 && larguraTela >= 480) {
+        
+        cols = 40;
+  }
+  else if (larguraTela <= 480 && larguraTela >= 390) {
+         cols = 30;
+  }
+  else if(larguraTela <= 390){
+        cols =20;
+  }
+
+  const textareas = document.querySelectorAll('textarea[cols]');
+  for (const textarea of textareas) {
+    textarea.cols = cols;
+  }
+}
+
+window.addEventListener('load', ajustarCols);
+window.addEventListener('resize', ajustarCols);
+
+
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', function () {
+  menu.classList.toggle('show');
+});
