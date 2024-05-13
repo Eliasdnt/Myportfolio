bd4a28029024c463ab085ea7241c20e529249d7b
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-05-13 16:18:41 -0300

    refatorei toda a section de projetos, add Swiper lib

---
+++ js/slide.js
@@ -0,0 +1,9 @@
+  var slider = new  Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        autoplay: {
+            delay: 4500,
+        }
+    })
